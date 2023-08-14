4a6f84dd09bf1fcd4fd7db3a3f8c5ec910c71335
sixthextinction <prithwish.nath@gmail.com>
2023-08-14 16:23:21 +0530

    redesign_prith

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,118 +3,70 @@ import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 
 const Summary = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        marginBottom: "1.5rem",
-        borderRadius: "0.375rem",
-        backgroundColor: "white",
-        padding: "1.5rem",
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-      }}
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="100%"
+      marginBottom="1.5rem"
+      borderRadius="0.375rem"
+      backgroundColor="white"
+      padding="1.5rem"
+      boxShadow="0px 2px 4px rgba(0, 0, 0, 0.1)"
     >
-      <h2
-        style={{ marginBottom: "1rem", fontSize: "1.5rem", fontWeight: "600" }}
-      >
+      <Heading marginBottom="1rem" fontSize="1.5rem" fontWeight="600">
         Summary
-      </h2>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: "1rem",
-        }}
-      >
-        <div
-          style={{
-            borderRadius: "0.375rem",
-            backgroundColor: "#F3F4F6",
-            padding: "1rem",
-          }}
+      </Heading>
+      <Grid templateColumns="repeat(3, 1fr)" gap="1rem">
+        <GridItem
+          borderRadius="0.375rem"
+          backgroundColor="#F3F4F6"
+          padding="1rem"
         >
-          <h3
-            style={{
-              marginBottom: "0.5rem",
-              fontSize: "1.25rem",
-              fontWeight: "600",
-            }}
-          >
+          <Heading marginBottom="0.5rem" fontSize="1.25rem" fontWeight="600">
             Total Users
-          </h3>
-          <div style={{ fontSize: "2.25rem", fontWeight: "700" }}>10</div>
-        </div>
-        <div
-          style={{
-            borderRadius: "0.375rem",
-            backgroundColor: "#F3F4F6",
-            padding: "1rem",
-          }}
+          </Heading>
+          <Text fontSize="2.25rem" fontWeight="700">
+            10
+          </Text>
+        </GridItem>
+        <GridItem
+          borderRadius="0.375rem"
+          backgroundColor="#F3F4F6"
+          padding="1rem"
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-            }}
-          >
-            <h3
-              style={{
-                marginBottom: "0.5rem",
-                fontSize: "1.25rem",
-                fontWeight: "600",
-              }}
-            >
-              New Users (Q3 23)
-            </h3>
-          </div>
-
-          <div style={{ fontSize: "2.25rem", fontWeight: "700" }}>
-            8
-            <span
-              style={{
-                color: "green",
-                fontFamily: "monospace",
-                fontSize: "1.5rem",
-              }}
-            >
-              {" "}
+          <Box display="flex" flexDirection="row">
+            <Heading marginBottom="0.5rem" fontSize="1.25rem" fontWeight="600">
+              New Users (Month)
+            </Heading>
+          </Box>
+          <div>
+            <Text fontSize="2.25rem" fontWeight="700">
+              8
+            </Text>
+            <Text color="green" fontFamily="monospace" fontSize="1.5rem">
               +300%
-            </span>
+            </Text>
           </div>
-        </div>
-        <div
-          style={{
-            borderRadius: "0.375rem",
-            backgroundColor: "#F3F4F6",
-            padding: "1rem",
-          }}
+        </GridItem>
+        <GridItem
+          borderRadius="0.375rem"
+          backgroundColor="#F3F4F6"
+          padding="1rem"
         >
-          <h3
-            style={{
-              marginBottom: "0.5rem",
-              fontSize: "1.25rem",
-              fontWeight: "600",
-            }}
-          >
+          <Heading marginBottom="0.5rem" fontSize="1.25rem" fontWeight="600">
             Revenue
-          </h3>
-          <div style={{ fontSize: "2.25rem", fontWeight: "700" }}>
-            $12,345
-            <span
-              style={{
-                color: "green",
-                fontFamily: "monospace",
-                fontSize: "1.5rem",
-              }}
-            >
-              {" "}
+          </Heading>
+          <div>
+            <Text fontSize="2.25rem" fontWeight="700">
+              $12,345
+            </Text>
+            <Text color="green" fontFamily="monospace" fontSize="1.5rem">
               +78%
-            </span>
+            </Text>
           </div>
-        </div>
-      </div>
-    </div>
+        </GridItem>
+      </Grid>
+    </Box>
   );
 };
 
